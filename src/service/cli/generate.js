f810be811fa8944915dfcc5b12de40c1bf335904
verbaux <verbaux@yandex.ru>
2020-03-12 20:11:09 +0300

    Module2-task2 Read template data for mock from files

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -7,23 +7,31 @@ const {
   DEFAULT_COUNT,
   MAX_COUNT,
   FILE_NAME,
+  FILE_CATEGORIES_PATH,
+  FILE_TITLES_PATH,
+  FILE_SENTENCES_PATH,
   MAX_ANNOUNCE_SENTENCES,
   RETROSPECTIVE_MS,
   ExitCode
 } = require(`../../constants`);
-const {
-  title: TITLES,
-  sentences: SENTENCES,
-  categories: CATEGORIES,
-} = require(`../../constants/data`);
 
-const generateRecords = (count) => (
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.split(`\n`).filter(Boolean);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
+
+const generateRecords = (count, titles, categories, sentences) => (
   Array(count).fill({}).map(() => ({
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     createdDate: new Date(Date.now() - getRandomInt(0, RETROSPECTIVE_MS)),
-    announce: shuffle(SENTENCES).slice(0, getRandomInt(1, MAX_ANNOUNCE_SENTENCES)).join(` `),
-    fullText: shuffle(SENTENCES).slice(0, getRandomInt(1, SENTENCES.length - 1)).join(` `),
-    category: shuffle(CATEGORIES).slice(0, getRandomInt(1, CATEGORIES.length - 1)),
+    announce: shuffle(sentences).slice(0, getRandomInt(1, MAX_ANNOUNCE_SENTENCES)).join(` `),
+    fullText: shuffle(sentences).slice(0, getRandomInt(1, sentences.length - 1)).join(` `),
+    category: shuffle(categories).slice(0, getRandomInt(1, categories.length - 1)),
   }))
 );
 
@@ -37,7 +45,12 @@ module.exports = {
       console.error(chalk.red(`Attention! No more than ${MAX_COUNT} records`));
       process.exit(ExitCode.error);
     }
-    const content = JSON.stringify(generateRecords(recordCount));
+
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+
+    const content = JSON.stringify(generateRecords(recordCount, titles, categories, sentences));
 
     try {
       await fs.writeFile(FILE_NAME, content);
